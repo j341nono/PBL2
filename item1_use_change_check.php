f4b1087e64076c84c5fedb27dda399abc921ee4d
j431miyo <j520431k@mails.cc.ehime-u.ac.jp>
2025-01-26 17:59:11 +0900

    Add files via upload

---
+++ item1_use_change_check.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+session_regenerate_id(true);
+if(isset($_SESSION['login'])==false)
+{
+    print 'ログインされていません<br/>';
+    print '<a href="../login/login.html">ログイン画面へ</a>';
+    exit();
+}
+else
+{
+    $userID=$_SESSION['userID'];
+    $name=$_SESSION['name'];
+}
+
+try {
+    $dsn = 'mysql:host=localhost;dbname=j431miyoP;charset=utf8';
+    $user = 'j431miyo';
+    $password = '';
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // ユーザーの現在のgoldとitem1を取得
+    $sql = 'SELECT item1_use FROM status2 WHERE userID = :userID';
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindValue(':userID', $userID, PDO::PARAM_INT);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+        if ($result) {
+            // item1_use +1
+            if($result['item1_use'] == 0){
+                $newItem1 = 1;
+            } else {
+                $newItem1 = 0;
+            }
+
+
+        // 更新処理
+        $sql = 'UPDATE status2 SET item1_use = :item1_use WHERE userID = :userID';
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindValue(':item1_use', $newItem1, PDO::PARAM_INT);
+        $stmt->bindValue(':userID', $userID, PDO::PARAM_INT);
+        $stmt->execute();
+        
+
+        print 'item1を購入しました<br/>';
+        print '<a href="game_start.php">ゲームスタートページへ</a>';
+        }
+    } else {
+        print '該当するユーザーが見つかりません<br/>';
+        print '<a href="../login/login.html">ログインページへ</a>';
+    }
+} catch (Exception $e) {
+    print 'ただいま障害が発生しております。<br/>';
+    print $e->getMessage();
+    exit();
+}
+
+?>
